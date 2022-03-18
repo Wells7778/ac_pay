771d3b731cc20c69806537f4c91c3c5d26aedc46
Wells <chawkeye@gmail.com>
2022-03-18 12:09:27 +0800

    init gem

---
+++ lib/ac_pay.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "ac_pay/version"
+
+module AcPay
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/ac_pay_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe AcPay do
+  it "has a version number" do
+    expect(AcPay::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
